e48a3f3d8c42b9ea34f9347d918660e6d3c6cadd
Minseok Choe <choems@outlook.com>
2024-11-29 00:39:51 +0900

    feat: artists page

---
+++ src/app/artists/(root)/page.tsx
@@ -0,0 +1,42 @@
+import { AUTH_TOKEN_COOKIE_NAME } from '@/constants/auth';
+import { cookies } from 'next/headers';
+import { ArtistsSchema } from '@/types/artist.type';
+import ArtistsTable from './table.page';
+
+async function getArtists() {
+  try {
+    const token = cookies().get(AUTH_TOKEN_COOKIE_NAME)?.value;
+
+    const response = await fetch(
+      new URL('/shuttle-operation/admin/artists', process.env.BASE_URL),
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        cache: 'no-cache',
+      },
+    );
+
+    const json = await response.json();
+
+    return ArtistsSchema.parse(json.artists);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+const ArtistPage = async () => {
+  try {
+    const a = await getArtists();
+    return (
+      <div>
+        <h2 className="text-[24px] font-500">아티스트 목록</h2>
+        <ArtistsTable artists={a} />
+      </div>
+    );
+  } catch (e) {
+    return JSON.stringify(e);
+  }
+};
+
+export default ArtistPage;
---
+++ src/app/artists/(root)/table.page.tsx
@@ -0,0 +1,90 @@
+'use client';
+import { ArtistType } from '@/types/artist.type';
+import { useReactTable } from '@tanstack/react-table';
+import { useState } from 'react';
+import { createColumnHelper } from '@tanstack/react-table';
+import { flexRender } from '@tanstack/react-table';
+import { getCoreRowModel } from '@tanstack/react-table';
+
+interface Props {
+  artists: ArtistType[];
+}
+
+const ArtistsTable = ({ artists }: Props) => {
+  const [data] = useState([...artists]);
+
+  // Columns and data are defined in a stable reference, will not cause infinite loop!
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <table className="text-sm w-full border-separate border-spacing-y-8 text-left text-grey-900 rtl:text-right">
+      <thead className="text-xs bg-grey-50 uppercase text-grey-700">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr
+            key={headerGroup.id}
+            className="
+          border-spacing-y-40 rounded-lg border border-b border-grey-50 bg-white transition-colors hover:bg-grey-50"
+          >
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr
+            key={row.id}
+            className="border-spacing-y-40 rounded-lg border border-b border-grey-50 bg-white transition-colors hover:bg-grey-50"
+          >
+            {row.getVisibleCells().map((cell) => (
+              <td
+                key={cell.id}
+                className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ArtistsTable;
+
+const columnHelper = createColumnHelper<ArtistType>();
+
+const columns = [
+  columnHelper.accessor('id', {
+    header: () => 'ID',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('name', {
+    header: () => '이름',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('createdAt', {
+    header: () => '생성일',
+    cell: (info) => info.getValue().toLocaleDateString(),
+  }),
+  columnHelper.accessor('updatedAt', {
+    header: () => '수정일',
+    cell: (info) => info.getValue().toLocaleDateString(),
+  }),
+];
