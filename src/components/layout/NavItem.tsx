928cf6efafb7e2e57c168d2e6cc038e5a0bc3b8b
Minseok Choe <choems@outlook.com>
2024-12-03 13:31:15 +0900

    refactor: code style

--- src/components/layout/NavItem.tsx
+++ src/components/layout/NavItem.tsx
@@ -4,20 +4,20 @@ import { type ReactNode } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-const NavItem = ({
-  href,
-  children,
-}: Readonly<{ href: string; children: ReactNode }>) => {
+interface Props {
+  href: string;
+  children: ReactNode;
+}
+
+const NavItem = ({ href, children }: Props) => {
   const firstHref = href.split('/').at(1);
   const firstPath = usePathname().split('/').at(1);
   return (
     <li>
       <Link
         href={href}
-        className={
-          `cursor-pointer break-keep transition-opacity hover:opacity-20 ` +
-          (firstPath === firstHref ? 'font-700' : 'font-400')
-        }
+        className={`cursor-pointer break-keep transition-opacity hover:opacity-20 
+          ${firstPath === firstHref ? 'font-700' : 'font-400'}`}
       >
         {children}
       </Link>
