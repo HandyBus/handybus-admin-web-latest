cda7956eb37d687767c8c83b4da10766ee366c1c
Minseok Choe <choems@outlook.com>
2024-12-26 03:13:47 +0900

    feat: fuzzy search utility

---
+++ src/utils/fuzzy.util.ts
@@ -0,0 +1,23 @@
+import { matches } from 'kled';
+
+export const filterByFuzzy = <T>(
+  list: T[],
+  query: string,
+  accessor: (t: T) => string,
+  threshold: number = 0,
+) => {
+  return list
+    .map((item) => {
+      const value = accessor(item);
+      return {
+        item,
+        score:
+          query.length > value.length
+            ? matches(value, query)
+            : matches(query, value),
+      };
+    })
+    .sort((a, b) => b.score - a.score)
+    .filter((item) => item.score > threshold)
+    .map((item) => item.item);
+};
