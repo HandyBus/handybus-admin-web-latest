3aad0ffb343228bf58ec84fabe85623e33be621f
Minseok Choe <choems@outlook.com>
2024-12-26 20:19:59 +0900

    fix: typo

--- src/app/shuttles/[shuttle_id]/dates/[daily_id]/routes/[route_id]/buses/new/layout.tsx
+++ src/app/shuttles/[shuttle_id]/dates/[daily_id]/routes/[route_id]/buses/new/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: Props) => {
   return (
     <main className="flex h-full w-full flex-col gap-16 bg-white">
       <header className="flex flex-row justify-between items-center">
-        <h1 className="text-[32px] font-500">거점지 추가</h1>
+        <h1 className="text-[32px] font-500">버스 추가</h1>
       </header>
       {children}
     </main>
