8fdd7c51b7c838aa8962707c2976653df5ba140c
Minseok Choe <choems@outlook.com>
2024-12-27 12:13:55 +0900

    fix: 아티스트 조회 및 추가 페이지 경미한 수정 (#12)
    * fix: api 대응
    
    * feat: add kakao map type
    
    * init: add korean fuzzy search library
    
    * feat: add styled table components
    
    * feat: tanstack data table component
    
    * fix: types
    
    * fix: api actions
    
    * feat: fuzzy search utility
    
    * feat: date utility
    
    * fix: date format
    
    * fix: build issue
    
    * feat: hubs
    
    * rename: types
    
    * fix: squash
    
    * fix: artist
    
    * remove: unused
    
    * fix: use handle-submit of react hook form
    
    * fix: use data table

--- src/app/artists/(home)/page.tsx
+++ src/app/artists/(home)/page.tsx
@@ -1,29 +1,6 @@
-import { AUTH_TOKEN_COOKIE_NAME } from '@/constants/auth';
-import { cookies } from 'next/headers';
-import { ArtistListSchema } from '@/types/artist.type';
-import ArtistsTable from './components/ArtistTable';
-
-async function getArtists() {
-  try {
-    const token = cookies().get(AUTH_TOKEN_COOKIE_NAME)?.value;
-
-    const response = await fetch(
-      new URL('/shuttle-operation/admin/artists', process.env.BASE_URL),
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        cache: 'no-cache',
-      },
-    );
-
-    const json = await response.json();
-
-    return ArtistListSchema.parse(json.artists);
-  } catch (e) {
-    return Promise.reject(e);
-  }
-}
+import DataTable from '@/components/table/DataTable';
+import { getArtists } from '@/app/actions/artists.action';
+import { columns } from './\btypes/table.type';
 
 const ArtistPage = async () => {
   try {
@@ -31,7 +8,7 @@ const ArtistPage = async () => {
     return (
       <div>
         <h2 className="text-[24px] font-500">아티스트 목록</h2>
-        <ArtistsTable artists={artists} />
+        <DataTable data={artists} columns={columns} />
       </div>
     );
   } catch (e) {
--- src/app/artists/new/page.tsx
+++ src/app/artists/new/page.tsx
@@ -1,54 +1,55 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useCallback, type FormEventHandler } from 'react';
-import { useForm } from 'react-hook-form';
+import { useCallback } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { addArtist } from '@/app/actions/artists.action';
-import TextInput from '@/components/text-input/TextInput';
+import { PlusIcon } from 'lucide-react';
+import Input from '@/components/input/Input';
 
 const NewArtistPage = () => {
   const router = useRouter();
-  const { trigger, setValue, control, getValues } = useForm<{ name: string }>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{
+    name: string;
+  }>({
     defaultValues: {
       name: '',
     },
   });
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(
-    (e) => {
-      e.preventDefault();
-      const { name } = getValues();
+  const onSubmit = useCallback(
+    ({ name }: { name: string }) => {
       if (confirm(`"${name}" 아티스트를 추가하시겠습니까?`)) {
-        (async () => {
-          const isStepValid = await trigger(['name']);
-          if (!isStepValid) {
-            return;
-          }
-          addArtist(name)
-            .then(() => {
-              alert('아티스트가 추가되었습니다.');
-            })
-            .catch((error) => {
-              const stack =
-                error instanceof Error ? error.stack : 'Unknown Error';
-              alert(`아티스트 추가에 실패했습니다. 스택: ${stack}`);
-            })
-            .finally(() => router.push('/artists'));
-        })();
+        addArtist(name)
+          .then(() => {
+            alert('아티스트가 추가되었습니다.');
+          })
+          .catch((error) => {
+            const stack =
+              error instanceof Error ? error.stack : 'Unknown Error';
+            alert(`아티스트 추가에 실패했습니다. 스택: ${stack}`);
+          })
+          .finally(() => router.push('/artists'));
       }
     },
-    [getValues, router, trigger],
+    [router],
   );
 
   return (
     <main className="h-full w-full bg-white flex flex-col gap-16">
       <h2 className="text-24 font-500">아티스트 정보</h2>
-      <form onSubmit={handleSubmit} method="post">
-        <TextInput
-          name="name"
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+        method="post"
+      >
+        <Controller
           control={control}
-          setValue={setValue}
-          placeholder="아티스트 이름을 입력해주세요."
+          name="name"
           rules={{
             required: '필수 입력',
             pattern: {
@@ -56,11 +57,22 @@ const NewArtistPage = () => {
               message: '공백으로만 이루어진 입력은 허용되지 않습니다.',
             },
           }}
-        >
-          이름
-        </TextInput>
+          render={({ field: { onChange, value } }) => (
+            <>
+              <Input
+                value={value}
+                setValue={onChange}
+                placeholder="아티스트 이름을 입력해주세요."
+              />
+              {errors?.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
+            </>
+          )}
+        />
+
         <button type="submit" className="rounded-lg bg-blue-500 p-8 text-white">
-          추가
+          <PlusIcon />
         </button>
       </form>
     </main>
