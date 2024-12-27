db951bb5b5aec8b81e83d5ef3e5d8810a91524e1
Minseok Choe <choems@outlook.com>
2024-12-27 12:20:14 +0900

    rename: layout

--- src/app/shuttles/new/layout.tsx
+++ src/app/shuttles/new/layout.tsx
@@ -3,7 +3,7 @@ interface Props {
   children: ReactNode;
 }
 
-const Page = ({ children }: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <main className="flex h-full w-full flex-col gap-16 bg-white">
       <h1 className="text-[32px] font-500">셔틀 추가하기</h1>
@@ -12,4 +12,4 @@ const Page = ({ children }: Props) => {
   );
 };
 
-export default Page;
+export default Layout;
