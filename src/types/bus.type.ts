e91e6ff71e38203ed79daaec346f00d91f70a1f0
Minseok Choe <choems@outlook.com>
2024-12-30 16:39:25 +0900

    fix: fixup - types

--- src/types/bus.type.ts
+++ src/types/bus.type.ts
@@ -2,33 +2,33 @@ import { z } from 'zod';
 
 const phoneRegex = /^01\d\-\d{4}\-d{4}$/;
 
-const carNumberRegex = /^[0-9]{2}[가-힣]{1}[0-9]{4}$/;
+// const carNumberRegex = /^[0-9]{2}[가-힣]{1}[0-9]{4}$/;
 
-const zodPhoneNumber = z
-  .string()
-  .refine(
-    (value) => phoneRegex.test(value),
-    '010으로 시작하는 11자리 숫자를 입력해주세요',
-  );
+// const zodPhoneNumber = z
+//   .string()
+//   .refine(
+//     (value) => phoneRegex.test(value),
+//     '010으로 시작하는 11자리 숫자를 입력해주세요',
+//   );
 
-const zodCarNumber = z
-  .string()
-  .refine((value) => carNumberRegex.test(value), '차량번호를 입력해주세요');
+// const zodCarNumber = z
+//   .string()
+//   .refine((value) => carNumberRegex.test(value), '차량번호를 입력해주세요');
 
-const zodOpenChatLink = z
-  .string()
-  .url()
-  .startsWith('https://openchat.kakao.com');
+// const zodOpenChatLink = z
+//   .string()
+//   .url()
+//   .startsWith('https://openchat.kakao.com');
 
 export const BusSchema = z.object({
   shuttleBusId: z.number().int(),
   shuttleRouteId: z.number().int(),
-  handyUserId: z.number().int(), // is this nullable?
+  handyUserId: z.number().int().nullable(), // is this nullable?
   type: z.enum(['SEATER_12', 'SEATER_28', 'BUS_45']),
-  name: z.number().int(),
-  number: zodCarNumber,
-  phoneNumber: zodPhoneNumber,
-  openChatLink: zodOpenChatLink,
+  name: z.string(),
+  number: z.unknown(), // zodCarNumber.optional(),
+  phoneNumber: z.unknown(), //zodPhoneNumber.optional(),
+  openChatLink: z.unknown(), //zodOpenChatLink.optional(),
   capacity: z.number().int(),
 });
 
