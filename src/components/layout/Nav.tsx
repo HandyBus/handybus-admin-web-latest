0f5707002348cf847aa806cf57c2d42c2a0339f4
Minseok Choe <choems@outlook.com>
2024-12-30 16:54:50 +0900

    fix: typo

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
       <ul className="gap-16 rounded-lg border border-primary-main bg-white p-20 text-18 text-black">
         <NavItem href="/">홈</NavItem>
         <NavItem href="/shuttles">셔틀</NavItem>
-        <NavItem href="/revervations">예약</NavItem>
+        <NavItem href="/reservations">예약</NavItem>
         <NavItem href="/hubs">거점지</NavItem>
         <NavItem href="/artists">아티스트</NavItem>
         <NavItem href="/coupons">쿠폰</NavItem>
