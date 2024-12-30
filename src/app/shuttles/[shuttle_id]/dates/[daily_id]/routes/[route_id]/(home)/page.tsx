d03524a562d67765ca8e66c5067644050a81279d
Minseok Choe <choems@outlook.com>
2024-12-30 16:47:28 +0900

    fix: route view page

--- src/app/shuttles/[shuttle_id]/dates/[daily_id]/routes/[route_id]/(home)/page.tsx
+++ src/app/shuttles/[shuttle_id]/dates/[daily_id]/routes/[route_id]/(home)/page.tsx
@@ -1,7 +1,8 @@
 import BlueLink from '@/components/link/BlueLink';
 import { getRoute } from '@/app/actions/route.action';
 import DataTable from '@/components/table/DataTable';
-import { columns } from './types/table.type';
+import { busColumns, routeHubColumns } from './types/table.type';
+import ConfirmRouteDialog from './ConfirmRouteDialog';
 
 interface Props {
   params: { shuttle_id: string; daily_id: string; route_id: string };
@@ -15,20 +16,67 @@ const Page = async ({ params: { shuttle_id, daily_id, route_id } }: Props) => {
       <header className="flex flex-row justify-between">
         <h1 className="text-[32px] font-500">노선 상세</h1>
       </header>
+      <div className="flex flex-row flex-wrap gap-4 rounded-lg border border-grey-100 p-8 text-14">
+        <BlueLink href={`/shuttles/${route.shuttleId}`}>셔틀</BlueLink>
+        <BlueLink
+          href={`/shuttles/${route.shuttleId}/dates/${route.dailyShuttleId}`}
+        >
+          일일 셔틀
+        </BlueLink>
+        <ConfirmRouteDialog shuttleRouteDetail={route} />
+      </div>
       <div className="flex flex-col gap-16">
-        <pre className="p-8 bg-grey-50 rounded-lg">
-          {JSON.stringify(route.shuttleRouteDetail, null, 2)}
-        </pre>
+        <div className="grid grid-cols-4 gap-4 bg-grey-50 p-16 rounded-xl">
+          <ul>
+            <li>노선 ID: {route.shuttleRouteId}</li>
+            <li>노선 이름: {route.name}</li>
+            <li>최대 승객 수: {route.maxPassengerCount}</li>
+          </ul>
+          <ul>
+            <li>얼리버드 여부: {route.hasEarlyBird ? 'O' : 'X'}</li>
+            {route.hasEarlyBird && (
+              <>
+                <li>
+                  예약 마감일: {route.earlybirdDeadline.toLocaleDateString()}
+                </li>
+                <li>
+                  귀가행 얼리버드 가격: {route.earlybirdPriceFromDestination}
+                </li>
+                <li>
+                  목적지행 얼리버드 가격: {route.earlybirdPriceToDestination}
+                </li>
+                <li>왕복 가격: {route.earlybirdPriceRoundTrip}</li>
+              </>
+            )}
+          </ul>
+          <ul>
+            <li>귀가행 가격: {route.regularPriceFromDestination}</li>
+            <li>목적지행 가격: {route.regularPriceToDestination}</li>
+            <li>왕복 가격: {route.regularPriceRoundTrip}</li>
+          </ul>
+          <ul>
+            <li>상태: {route.status}</li>
+          </ul>
+        </div>
 
         <header className="flex flex-row justify-between">
-          <h1 className="text-[24px] font-500">버스 목록</h1>
+          <h3 className="text-[24px] font-500">거점지 - 목적지행</h3>
+        </header>
+        <DataTable columns={routeHubColumns} data={route.hubs.toDestination} />
+
+        <header className="flex flex-row justify-between">
+          <h3 className="text-[24px] font-500">거점지 - 귀가행</h3>
+        </header>
+        <DataTable
+          columns={routeHubColumns}
+          data={route.hubs.fromDestination}
+        />
+
+        <header className="flex flex-row justify-between">
+          <h3 className="text-[24px] font-500">버스 목록 ({0})</h3>
           <BlueLink href={`${route_id}/buses/new`}>추가하기</BlueLink>
         </header>
-
-        <DataTable
-          columns={columns}
-          data={route.shuttleRouteDetail.shuttleBuses}
-        />
+        <DataTable columns={busColumns} data={route.shuttleBuses} />
       </div>
     </main>
   );
