34a4a7a40bcc4ffd8ec3c7a35646245d087d296e
Minseok Choe <choems@outlook.com>
2025-01-02 21:40:25 +0900

    fix: use previous hook

--- src/hooks/usePrevious.tsx
+++ src/hooks/usePrevious.tsx
@@ -4,24 +4,21 @@ import { useEffect, useRef } from 'react';
 
 const INITIAL_VALUE = Symbol('initialValue');
 
-const usePrevious = <T,>(
-  value: T,
-):
-  | { isFirst: true; hasPrevious: false; current: unknown }
-  | {
-      isFirst: false;
-      hasPrevious: true;
-      current: T;
-    } => {
+export const isFirst = <T,>(
+  value: T | typeof INITIAL_VALUE,
+): value is typeof INITIAL_VALUE => value === INITIAL_VALUE;
+
+export const hasPrevious = <T,>(value: T | typeof INITIAL_VALUE): value is T =>
+  value !== INITIAL_VALUE;
+
+const usePrevious = <T,>(value: T): T | typeof INITIAL_VALUE => {
   const ref = useRef<T | typeof INITIAL_VALUE>(INITIAL_VALUE);
 
   useEffect(() => {
     ref.current = value;
   }, [value]);
 
-  return ref.current === INITIAL_VALUE
-    ? { isFirst: true, hasPrevious: false, current: null }
-    : { isFirst: false, hasPrevious: true, current: ref.current };
+  return ref.current;
 };
 
 export default usePrevious;
