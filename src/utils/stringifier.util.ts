9af21f4e6cbf37995036a4c1e540173dc6acd2b7
Minseok Choe <choems@outlook.com>
2025-01-02 21:30:50 +0900

    feat: stringifier - handy status type

---
+++ src/utils/stringifier.util.ts
@@ -0,0 +1,18 @@
+import { HandyStatusType } from '@/types/reservation.type';
+
+const Stringifier = Object.freeze({
+  handyStatus(v: HandyStatusType) {
+    switch (v) {
+      case 'NOT_SUPPORTED':
+        return '지원하지 않음';
+      case 'SUPPORTED':
+        return '지원함';
+      case 'DECLINED':
+        return '거절됨';
+      case 'ACCEPTED':
+        return '승인됨';
+    }
+  },
+});
+
+export default Stringifier;
