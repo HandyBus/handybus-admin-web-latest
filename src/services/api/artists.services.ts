665349091710dfe1214915d3bac7dd803e82a58a
Minseok Choe <choems@outlook.com>
2025-01-03 23:23:06 +0900

    rename: use client side service

--- src/services/api/artists.services.ts
+++ src/services/api/artists.services.ts
@@ -1,21 +1,23 @@
-'use server';
+'use client';
 
-import { instance } from '@/services/config';
+import { authInstance } from '../new-fetch';
 import { revalidatePath } from 'next/cache';
 import { ArtistListSchema } from '@/types/artist.type';
 
 export const addArtist = async (name: string) => {
-  const response = (
-    await instance.post<{ ok: boolean }>('/shuttle-operation/admin/artists', {
+  const response = await authInstance.post<{ ok: boolean }>(
+    '/shuttle-operation/admin/artists',
+    {
       name,
-    })
-  ).data;
+    },
+  );
   revalidatePath('/artists', 'layout');
   return response;
 };
 
 export const getArtists = async () => {
-  const response = await instance.get('/shuttle-operation/admin/artists');
-  console.log(response.data);
-  return ArtistListSchema.parse(response.data.artists);
+  const response = await authInstance.get<{ ok: boolean; artists: unknown }>(
+    '/shuttle-operation/admin/artists',
+  );
+  return ArtistListSchema.parse(response.artists);
 };
