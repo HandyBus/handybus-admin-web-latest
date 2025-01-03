c78ad9cc4de28781f7127d1924f2465e6125c8a1
Minseok Choe <choems@outlook.com>
2025-01-03 23:36:36 +0900

    rename: api services

--- src/services/api/reservations.services.ts
+++ src/services/api/reservations.services.ts
@@ -1,15 +1,14 @@
-'use server';
+'use client';
 
-import { instance } from '@/services/config';
 import {
   ReservationDetailSchema,
   ReservationSchema,
   type UpdateReservationRequestType,
 } from '@/types/reservation.type';
-import { AxiosError } from 'axios';
 import { PaginationResponseSchema } from '@/types/meta/pagination.type';
 import { PAGINATION_LIMIT } from '@/constants/config';
-import { revalidatePath } from 'next/cache';
+import { authInstance } from '../new-fetch';
+import { queryClient } from '@/components/Provider';
 
 const ReservationResponse = PaginationResponseSchema(
   ReservationSchema.array(),
@@ -24,61 +23,40 @@ export const getReservations = async (
   userNickname: string | undefined,
   passengerName: string | undefined,
 ) => {
-  try {
-    let url = `/shuttle-operation/admin/reservations?limit=${PAGINATION_LIMIT}`;
-    if (pageIndex !== undefined && pageIndex !== null)
-      url = url.concat(`&page=${pageIndex}`);
-    if (shuttleId !== undefined && shuttleId !== null)
-      url = url.concat(`&shuttleId=${shuttleId}`);
-    if (dailyShuttleId !== undefined && dailyShuttleId !== null)
-      url = url.concat(`&dailyShuttleId=${dailyShuttleId}`);
-    if (shuttleRouteId !== undefined && shuttleRouteId !== null)
-      url = url.concat(`&shuttleRouteId=${shuttleRouteId}`);
-    if (userNickname !== undefined && userNickname !== null)
-      url = url.concat(`&userNickname=${userNickname}`);
-    if (passengerName !== undefined && passengerName !== null)
-      url = url.concat(`&passengerName=${passengerName}`);
+  let url = `/shuttle-operation/admin/reservations?limit=${PAGINATION_LIMIT}`;
+  if (pageIndex !== undefined && pageIndex !== null)
+    url = url.concat(`&page=${pageIndex}`);
+  if (shuttleId !== undefined && shuttleId !== null)
+    url = url.concat(`&shuttleId=${shuttleId}`);
+  if (dailyShuttleId !== undefined && dailyShuttleId !== null)
+    url = url.concat(`&dailyShuttleId=${dailyShuttleId}`);
+  if (shuttleRouteId !== undefined && shuttleRouteId !== null)
+    url = url.concat(`&shuttleRouteId=${shuttleRouteId}`);
+  if (userNickname !== undefined && userNickname !== null)
+    url = url.concat(`&userNickname=${userNickname}`);
+  if (passengerName !== undefined && passengerName !== null)
+    url = url.concat(`&passengerName=${passengerName}`);
 
-    const ret = await instance.get(url);
-    return ReservationResponse.parse(ret.data);
-  } catch (e) {
-    if (e instanceof AxiosError && e.response) {
-      throw e.response.data;
-    }
-    throw e;
-  }
+  const ret = await authInstance.get(url);
+  return ReservationResponse.parse(ret);
 };
 
 export const getReservation = async (id: number) => {
-  try {
-    const ret = await instance.get(
-      `/shuttle-operation/admin/reservations/${id}`,
-    );
-    return ReservationDetailSchema.parse(ret.data.shuttleReservation);
-  } catch (e) {
-    if (e instanceof AxiosError && e.response) {
-      throw e.response.data;
-    }
-    throw e;
-  }
+  const ret = await authInstance.get<{
+    ok: boolean;
+    shuttleReservation: unknown;
+  }>(`/shuttle-operation/admin/reservations/${id}`);
+  return ReservationDetailSchema.parse(ret.shuttleReservation);
 };
 
 export const updateReservation = async (
   reservationId: number,
   input: UpdateReservationRequestType,
 ) => {
-  try {
-    console.log(JSON.stringify(input, null, 2));
-    const ret = await instance.put(
-      `/shuttle-operation/admin/reservations/${reservationId}`,
-      input,
-    );
-    revalidatePath(`/reservations/${reservationId}`, 'layout');
-    return ret.data;
-  } catch (e) {
-    if (e instanceof AxiosError && e.response) {
-      throw e.response.data;
-    }
-    throw e;
-  }
+  const ret = await authInstance.put(
+    `/shuttle-operation/admin/reservations/${reservationId}`,
+    input,
+  );
+  queryClient.invalidateQueries({ queryKey: ['reservations'] });
+  return ret;
 };
