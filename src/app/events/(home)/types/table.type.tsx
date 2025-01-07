ba0404edf06d2dee5046237273605b4a78321d66
Minseok Choe <choems@outlook.com>
2025-01-08 01:54:42 +0900

    fix: add more columns

--- src/app/events/(home)/types/table.type.tsx
+++ src/app/events/(home)/types/table.type.tsx
@@ -9,12 +9,13 @@ const columnHelper = createColumnHelper<EventsViewType>();
 
 export const columns = [
   columnHelper.accessor('eventId', {
-    header: () => 'ID',
+    id: 'eventId',
+    header: '이벤트 ID',
     cell: (info) => info.getValue(),
   }),
   columnHelper.display({
     id: 'image',
-    header: () => '포스터',
+    header: '포스터',
     cell: (props) => (
       <Image
         src={props.row.original.eventImageUrl}
@@ -28,14 +29,51 @@ export const columns = [
     minSize: 40, //enforced during column resizing
     maxSize: 40, //enforced during column resizing
   }),
+  columnHelper.accessor('eventStatus', {
+    id: 'eventStatus',
+    header: '이벤트 상태',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('regionId', {
+    id: 'regionId',
+    header: '지역',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('regionHubId', {
+    id: 'regionHubId',
+    header: '거점지',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('eventType', {
+    id: 'eventType',
+    header: '이벤트 종류',
+    cell: (info) => info.getValue(),
+  }),
   columnHelper.accessor('eventName', {
-    header: () => '이벤트 이름',
+    id: 'eventName',
+    header: '이벤트 이름',
     cell: (info) => info.getValue(),
   }),
+  // Accessor Column
   columnHelper.accessor('eventLocationName', {
-    header: () => '장소',
+    id: 'eventLocationName',
+    header: '이벤트 장소 이름',
     cell: (info) => info.getValue(),
   }),
+  columnHelper.accessor('eventLocationAddress', {
+    id: 'eventLocationAddress',
+    header: '이벤트 장소 주소',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor(
+    (e) =>
+      `(${e.eventLocationLatitude.toFixed(2)}… , ${e.eventLocationLongitude.toFixed(2)}…)`,
+    {
+      id: 'eventCoord',
+      header: '이벤트 장소 좌표',
+      cell: (info) => info.getValue(),
+    },
+  ),
   columnHelper.accessor((row) => row.dailyEvents.map((de) => de.date), {
     header: '날짜',
     cell: ({ getValue }) => {
@@ -51,6 +89,7 @@ export const columns = [
   }),
   // TODO remove option chaining '?' after fixing the api
   columnHelper.accessor((row) => row.eventArtists?.map((p) => p.artistName), {
+    id: 'artists',
     header: '출연자',
     cell: (info) => {
       const ps: string[] = info.getValue() || [];
@@ -65,7 +104,7 @@ export const columns = [
   }),
   columnHelper.display({
     id: 'actions  ',
-    header: () => '액션',
+    header: '액션',
     cell: (props) => (
       <BlueLink href={`/events/${props.row.original.eventId}`}>
         자세히 보기
