1f3a0aafdde6c63b5f6d98412ae7757b97d70fe5
Minseok Choe <choems@outlook.com>
2025-01-08 01:53:13 +0900

    feat: column visibility hook

--- src/components/table/DataTable.tsx
+++ src/components/table/DataTable.tsx
@@ -7,11 +7,13 @@ import {
   getCoreRowModel,
   ColumnDef,
   getFilteredRowModel,
+  VisibilityState,
 } from '@tanstack/react-table';
 
 interface Props<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  columnVisibility?: VisibilityState | undefined;
 }
 
 /**
@@ -25,6 +27,7 @@ interface Props<TData, TValue> {
 const DataTable = <TData,>({
   columns,
   data: dataSource,
+  columnVisibility,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 }: Props<TData, any>) => {
   const [data] = useState([...dataSource]);
@@ -33,52 +36,57 @@ const DataTable = <TData,>({
     columns,
     data,
     getCoreRowModel: getCoreRowModel(),
+    state: {
+      columnVisibility,
+    },
     getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
-    <table className="text-sm w-full border-separate border-spacing-y-8 text-left text-grey-900 rtl:text-right">
-      <thead className="text-xs bg-grey-50 uppercase text-grey-700">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr
-            key={headerGroup.id}
-            className="
+    <div className="w-full overflow-x-scroll">
+      <table className="text-sm border-separate border-spacing-y-8 text-left text-grey-900 rtl:text-right">
+        <thead className="text-xs bg-grey-50 uppercase text-grey-700">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr
+              key={headerGroup.id}
+              className="
           border-spacing-y-40 rounded-lg border border-b border-grey-50 bg-white transition-colors hover:bg-grey-50"
-          >
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
-              >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr
-            key={row.id}
-            className="border-spacing-y-40 rounded-lg border border-b border-grey-50 bg-white transition-colors hover:bg-grey-50"
-          >
-            {row.getVisibleCells().map((cell) => (
-              <td
-                key={cell.id}
-                className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+            >
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr
+              key={row.id}
+              className="border-spacing-y-40 rounded-lg border border-b border-grey-50 bg-white transition-colors hover:bg-grey-50"
+            >
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="border-y border-r border-grey-100 p-8 first:rounded-l-lg first:border-l last:rounded-r-lg"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
