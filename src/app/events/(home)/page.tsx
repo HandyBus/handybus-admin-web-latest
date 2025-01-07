49d27247b94131e9abcf052ca95e3f6ba1b6cf84
Minseok Choe <choems@outlook.com>
2025-01-07 21:03:16 +0900

    feat: use-column-visibility hook

--- src/app/events/(home)/page.tsx
+++ src/app/events/(home)/page.tsx
@@ -5,6 +5,7 @@ import DataTable from '@/components/table/DataTable';
 import { columns } from './types/table.type';
 import { getAllShuttles } from '@/services/v2/shuttleEvent.services';
 import { useQuery } from '@tanstack/react-query';
+import useColumnVisibility from '@/hooks/useColumnVisibility';
 
 const Page = () => {
   const {
@@ -17,18 +18,26 @@ const Page = () => {
     queryFn: getAllShuttles,
   });
 
+  const [columnVisibility, Filter] = useColumnVisibility(columns);
+
   return (
     <main className="flex h-full w-full flex-col gap-16 bg-white">
       <header className="flex flex-row justify-between">
         <h1 className="text-[32px] font-500">이벤트 대시보드</h1>
         <BlueLink href="events/new">추가하기</BlueLink>
       </header>
+      {/* <ColumnDisplay /> */}
+      <Filter columnVisibility={columnVisibility} />
       {isLoading ? (
         <div>Loading...</div>
       ) : isError ? (
         <div>Error: {error.message}</div>
       ) : shuttles ? (
-        <DataTable data={shuttles} columns={columns} />
+        <DataTable
+          data={shuttles}
+          columns={columns}
+          columnVisibility={columnVisibility}
+        />
       ) : (
         <div>No data</div>
       )}
