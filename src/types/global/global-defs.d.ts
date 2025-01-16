18f6b1df973b59256a95efc262950c0a9a609d83
Minseok Choe <choems@outlook.com>
2025-01-16 16:57:02 +0900

    feat: utility type

--- src/types/global/global-defs.d.ts
+++ src/types/global/global-defs.d.ts
@@ -3,4 +3,8 @@ export {};
 declare global {
   type ArrayElement<ArrayType extends readonly unknown[]> =
     ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+  type AllowUndefind<T, K extends keyof T> = Omit<T, K> & {
+    [P in K]: T[P] | undefined;
+  };
 }
