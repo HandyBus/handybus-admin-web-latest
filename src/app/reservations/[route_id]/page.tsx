266367989251383e7038ea8badc20fe0d31e0028
Geon <geonulim@gmail.com>
2025-01-17 12:00:40 +0900

    feat: 노션별 예약 페이지에 노선 정보 추가

--- src/app/reservations/[route_id]/page.tsx
+++ src/app/reservations/[route_id]/page.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
 import BusTable from './components/BusTable';
 import ReservationTable from './components/ReservationTable';
+import { getRoute } from '@/services/v2/shuttleRoute.services';
 
 interface Props {
   params: {
@@ -16,11 +20,37 @@ const Page = ({ params, searchParams }: Props) => {
   const dailyEventId = Number(searchParams.dailyEventId);
   const shuttleRouteId = Number(params.route_id);
 
+  const { data: shuttleRoute } = useQuery({
+    queryKey: ['shuttleRoute', shuttleRouteId],
+    queryFn: () => getRoute(eventId, dailyEventId, shuttleRouteId),
+  });
+
+  const eventName = shuttleRoute?.event?.eventName;
+  const dailyEventDate = new Date(
+    shuttleRoute?.event?.dailyEvents.find(
+      (dailyEvent) => dailyEvent.dailyEventId === dailyEventId,
+    )?.date ?? '',
+  ).toLocaleDateString();
+  const shuttleRouteName = shuttleRoute?.name;
+
   return (
     <>
       <main className="flex h-full w-full flex-col gap-16 bg-white">
-        <h1 className="text-[32px] font-500 flex items-center">
+        <h1 className="text-[32px] font-500 flex items-end">
           노선별 예약 관리
+          {shuttleRoute && (
+            <p className="flex items-center gap-8 ml-20">
+              <span className="text-16 font-500 text-grey-800">
+                {eventName}
+              </span>
+              <span className="text-14 font-400 text-grey-600">
+                {dailyEventDate}
+              </span>
+              <span className="text-14 font-500 text-grey-700">
+                {shuttleRouteName}
+              </span>
+            </p>
+          )}
         </h1>
         <ReservationTable
           eventId={eventId}
