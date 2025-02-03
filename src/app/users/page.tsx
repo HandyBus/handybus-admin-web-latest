07c31b792b0ab63adf00c0a8839e15909d53bede
Geon <geonulim@gmail.com>
2025-02-03 15:25:46 +0900

    feat: 유저 테이블 무한 스크롤 적용

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,13 +7,26 @@ import { useMemo } from 'react';
 import Heading from '@/components/text/Heading';
 import BaseTable from '@/components/table/BaseTable';
 import Callout from '@/components/text/Callout';
+import { PAGINATION_LIMIT } from '@/constants/config';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
 const Page = () => {
-  const { data: users } = useGetUsersWithPagination();
+  const {
+    data: users,
+    hasNextPage,
+    fetchNextPage,
+  } = useGetUsersWithPagination({
+    page: undefined,
+    limit: PAGINATION_LIMIT,
+  });
+
+  const ref = useInfiniteScroll(fetchNextPage);
+
   const flattenedUsers = useMemo(
     () => users?.pages.flatMap((page) => page.users) || [],
     [users],
   );
+  const totalUserCount = users?.pages?.[0]?.totalCount;
 
   const table = useTable({
     columns,
@@ -25,9 +38,15 @@ const Page = () => {
     <main className="flex flex-col">
       <Heading>유저 대시보드</Heading>
       <Callout>
-        성별, 연령대, 지역이 없는 유저들은 온보딩을 완료하지 않은 유저들입니다.
+        총 유저 수: <b>{totalUserCount}</b>
+        <br />
+        <span className="text-14">
+          성별, 연령대, 지역이 없는 유저들은 온보딩을 완료하지 않은
+          유저들입니다.
+        </span>
       </Callout>
       <BaseTable table={table} />
+      {hasNextPage && <div ref={ref} />}
     </main>
   );
 };
