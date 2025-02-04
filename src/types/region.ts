6730662b34ac37110bbe8e55514f2248e176f05f
Geon <geonulim@gmail.com>
2025-02-04 16:52:06 +0900

    chore: 지역 파일 id를 string으로 수정

--- src/types/region.ts
+++ src/types/region.ts
@@ -1,13 +1,12 @@
 import { z } from 'zod';
 
-// TODO 추후 마이그레이션
 export const RegionSchema = z.object({
   regionId: z.string(),
   provinceFullName: z.string(),
   provinceShortName: z.string(),
   cityFullName: z.string(),
   cityShortName: z.string(),
-  relatedRegionIds: z.array(z.number().int()),
+  relatedRegionIds: z.string().array(),
   createdAt: z.string(),
   updatedAt: z.string(),
 });
