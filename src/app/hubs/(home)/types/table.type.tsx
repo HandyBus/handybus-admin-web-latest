649e094a098578a51f2881ee653b25ad90d4a95d
jujeon <jwjsjwnsstjd@gmail.com>
2025-02-04 18:08:06 +0900

    feat: 거점지 수정

--- src/app/hubs/(home)/types/table.type.tsx
+++ src/app/hubs/(home)/types/table.type.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BlueLink from '@/components/link/BlueLink';
 import { RegionHub } from '@/types/hub.type';
 import { createColumnHelper } from '@tanstack/react-table';
 
@@ -17,5 +18,30 @@ export const columns = [
   columnHelper.accessor('name', {
     header: () => '이름',
     cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('address', {
+    header: () => '주소',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('latitude', {
+    header: () => '위도',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('longitude', {
+    header: () => '경도',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.display({
+    id: 'actions',
+    header: () => 'actions',
+    cell: (info) => (
+      <div className="gap-10 flex">
+        <BlueLink
+          href={`/hubs/${info.row.original.regionId}/${info.row.original.regionHubId}/edit`}
+        >
+          수정하기
+        </BlueLink>
+      </div>
+    ),
   }),
 ];
