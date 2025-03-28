d02392b47fe1acb941bc830611ea0ce5b9ffaa56
jujeon <jwjsjwnsstjd@gmail.com>
2025-03-28 16:42:20 +0900

    feat(노선수정하기): 가는편/오는편에 같은 정류장을 가졌는지 검사로직 추가 및 리팩토링

---
+++ src/app/events/[eventId]/dates/[dailyEventId]/routes/[shuttleRouteId]/edit/utils/validateShuttleRouteData.ts
@@ -0,0 +1,77 @@
+import { UpdateShuttleRouteRequest } from '@/types/shuttleRoute.type';
+import dayjs from 'dayjs';
+
+export const validateShuttleRouteData = (
+  forwardHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+  returnHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+): void => {
+  if (!hasHubs(forwardHubs, returnHubs)) {
+    throw new Error('가는편/오는편의 정류장을 채워주세요.');
+  }
+  if (
+    !validateEachSequenceOrder(forwardHubs) ||
+    !validateEachSequenceOrder(returnHubs) ||
+    !validateFromToOrder(forwardHubs, returnHubs)
+  ) {
+    throw new Error('정류장들의 순서가 올바르지 않습니다.');
+  }
+  if (!validateHubsMatch(forwardHubs, returnHubs)) {
+    throw new Error('정류장들의 목록이 서로 일치하지 않습니다.');
+  }
+};
+
+const hasHubs = (
+  forwardHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+  returnHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+) => {
+  return forwardHubs.length > 0 && returnHubs.length > 0;
+};
+
+const validateEachSequenceOrder = (
+  hubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+) => {
+  const sortedByArrivalTime = [...hubs].sort(
+    (a, b) => dayjs(a.arrivalTime).valueOf() - dayjs(b.arrivalTime).valueOf(),
+  );
+  const isSequenceValid = hubs.every(
+    (hub, index) => hub.sequence === sortedByArrivalTime[index].sequence,
+  );
+  if (!isSequenceValid) {
+    return false;
+  }
+  return true;
+};
+
+const validateFromToOrder = (
+  forwardHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+  returnHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+) => {
+  const forwardLastArrivalTime = Math.max(
+    ...forwardHubs.map((hub) => dayjs(hub.arrivalTime).valueOf()),
+  );
+
+  const returnFirstArrivalTime = Math.min(
+    ...returnHubs.map((hub) => dayjs(hub.arrivalTime).valueOf()),
+  );
+
+  if (forwardLastArrivalTime >= returnFirstArrivalTime) {
+    return false;
+  }
+  return true;
+};
+
+const validateHubsMatch = (
+  forwardHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+  returnHubs: UpdateShuttleRouteRequest['shuttleRouteHubs'],
+) => {
+  const forwardHubIds = forwardHubs.map((hub) => hub.regionHubId);
+  const returnHubIds = returnHubs.map((hub) => hub.regionHubId);
+
+  if (
+    forwardHubIds.every((forwardHub) => returnHubIds.includes(forwardHub)) &&
+    returnHubIds.every((returnHub) => forwardHubIds.includes(returnHub))
+  ) {
+    return true;
+  }
+  return false;
+};
