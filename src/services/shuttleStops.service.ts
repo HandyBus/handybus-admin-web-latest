6444d9c7d7a0a7200a7eb54123f6d9b711f00b3c
jujeon <jwjsjwnsstjd@gmail.com>
2025-04-01 11:21:24 +0900

    refactor: 사용하지 않는 POST api 제거

--- src/services/shuttleStops.service.ts
+++ src/services/shuttleStops.service.ts
@@ -1,20 +1,9 @@
 import { silentParse } from '@/utils/parse.util';
 import { authInstance } from './config';
 import {
-  AdminCreateShuttleStopRequest,
   AdminSaveShuttleStopTagRequest,
   AdminSaveShuttleStopTagRequestSchema,
 } from '@/types/shuttleStop.type';
-import { AdminCreateShuttleStopRequestSchema } from '@/types/shuttleStop.type';
-
-// ----- POST -----
-
-export const postShuttleStop = async (data: AdminCreateShuttleStopRequest) => {
-  await authInstance.post(
-    '/v1/shuttle-operation/admin/shuttle-stops',
-    silentParse(AdminCreateShuttleStopRequestSchema, data),
-  );
-};
 
 // ----- PUT -----
 
--- src/types/shuttleStop.type.ts
+++ src/types/shuttleStop.type.ts
@@ -1,14 +1,5 @@
 import { z } from 'zod';
 
-export const AdminCreateShuttleStopRequestSchema = z.object({
-  regionHubId: z.string(),
-  type: z.enum(['EVENT_DESTINATION', 'SHUTTLE_HUB']),
-});
-
-export type AdminCreateShuttleStopRequest = z.infer<
-  typeof AdminCreateShuttleStopRequestSchema
->;
-
 export const AdminSaveShuttleStopTagRequestSchema = z.object({
   regionHubId: z.string(),
   types: z.array(z.enum(['EVENT_DESTINATION', 'SHUTTLE_HUB'])),
