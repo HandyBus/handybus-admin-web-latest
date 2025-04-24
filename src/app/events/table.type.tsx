a56c5a1214ae887f9b1269ab22537dbe3d299c57
jujeon <jwjsjwnsstjd@gmail.com>
2025-04-24 17:55:39 +0900

    style: 날짜 정렬에서Date -> dayjs 로 변경

--- src/app/events/table.type.tsx
+++ src/app/events/table.type.tsx
@@ -7,6 +7,7 @@ import { EventWithStatisticsViewEntity } from '@/types/event.type';
 import { DEFAULT_EVENT_IMAGE } from '@/constants/common';
 import Stringifier from '@/utils/stringifier.util';
 import BlueLink from '@/components/link/BlueLink';
+import dayjs from 'dayjs';
 
 const columnHelper = createColumnHelper<EventWithStatisticsViewEntity>();
 
@@ -49,9 +50,7 @@ export const columns = [
   columnHelper.accessor(
     (row) =>
       row.dailyEvents
-        .toSorted(
-          (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
-        )
+        .toSorted((a, b) => dayjs(a.date).diff(dayjs(b.date)))
         .map((dailyEvent) => dailyEvent.date),
     {
       header: '날짜',
