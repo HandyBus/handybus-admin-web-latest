4e4fb7ec6fa593b91f9bcfe618bbfac3abed2305
Geon <geonulim@gmail.com>
2025-07-01 17:28:24 +0900

    feat: 핸디팟 정류장 생성 및 조회

--- src/types/shuttleStop.type.ts
+++ src/types/shuttleStop.type.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod';
+import { HubUsageTypeEnum } from './hub.type';
 
 export const AdminSaveShuttleStopTagRequestSchema = z.object({
   regionHubId: z.string(),
-  types: z.array(
-    z.enum(['EVENT_LOCATION', 'EVENT_PARKING_LOT', 'SHUTTLE_HUB']),
-  ),
+  types: z.array(HubUsageTypeEnum),
 });
 
 export type AdminSaveShuttleStopTagRequest = z.infer<
