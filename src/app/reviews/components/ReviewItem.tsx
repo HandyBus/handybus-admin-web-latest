66080d0603100ab4a020d73e266f8b542968471c
jujeon <jwjsjwnsstjd@gmail.com>
2025-09-29 10:40:59 +0900

    feat: 리뷰 보기 페이지 (그대로가져옴)

---
+++ src/app/reviews/components/ReviewItem.tsx
@@ -0,0 +1,134 @@
+import { useCallback, useRef, useState } from 'react';
+import Image from 'next/image';
+import ImageModal from './ImageModal';
+import { ReviewsViewEntityProduct } from '@/types/reviews.type';
+import UserProfile from './UserProfile';
+import Rating from './Rating';
+import ReviewProperty from './ReviewProperty';
+import { formatDateString } from '@/utils/date.util';
+
+interface Props {
+  review: ReviewsViewEntityProduct;
+}
+
+const ReviewItem = ({ review }: Props) => {
+  const [openImageUrl, setOpenImageUrl] = useState<string | null>(null);
+  const [clamped, setClamped] = useState(false);
+  const [useClamp, setUseClamp] = useState(true);
+
+  const prevWidth = useRef(0);
+
+  const ref = useCallback(
+    (node: HTMLParagraphElement | null) => {
+      if (node !== null) {
+        const resizeObserver = new ResizeObserver((entries) => {
+          for (const entry of entries) {
+            if (entry.target.clientWidth === prevWidth.current) continue;
+            if (!useClamp) continue;
+            prevWidth.current = entry.target.clientWidth;
+            setClamped(entry.target.scrollHeight > entry.target.clientHeight);
+          }
+        });
+        resizeObserver.observe(node);
+
+        return () => {
+          resizeObserver.unobserve(node);
+        };
+      }
+    },
+    [useClamp],
+  );
+
+  const blurredName = review.userName?.slice(0, 1) + '**';
+
+  const getDisplayName = () => {
+    if (review.userName) {
+      return blurredName;
+    }
+    if (review.userNickname) {
+      return review.userNickname;
+    }
+    return '핸디버스 탑승객';
+  };
+
+  const displayName = getDisplayName();
+
+  return (
+    <>
+      <article>
+        <div key={review.reviewId} className={`flex flex-col rounded-8 p-12`}>
+          <div className="flex flex-col items-start gap-[2px]">
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <UserProfile
+                  name={review.userName || review.userNickname}
+                  profileImage={review.userProfileImage}
+                />
+                <p className="text-12 font-500 leading-[160%] text-basic-black">
+                  {displayName}
+                </p>
+              </div>
+              <Rating size="medium" value={review.rating} />
+            </div>
+            <ReviewProperty review={review} />
+            {review.reviewImages && review.reviewImages.length > 0 && (
+              <figure className="flex gap-4 py-4">
+                {review.reviewImages?.map((image, index) => {
+                  return (
+                    <button
+                      type="button"
+                      className="relative h-60 w-60"
+                      key={index}
+                      onClick={() => {
+                        setOpenImageUrl(image.imageUrl);
+                      }}
+                    >
+                      <Image
+                        src={image.imageUrl}
+                        alt={`${review.eventName} 후기 이미지 ${index + 1}`}
+                        fill
+                        className="rounded-8 object-cover"
+                      />
+                    </button>
+                  );
+                })}
+              </figure>
+            )}
+            <p
+              ref={ref}
+              className={`overflow-hidden text-14 font-500 leading-[160%] text-basic-grey-600 ${
+                useClamp ? 'line-clamp-2' : ''
+              }`}
+            >
+              {review.content}
+            </p>
+            {clamped && (
+              <button
+                className="text-14 font-500 leading-[160%] text-basic-grey-500"
+                onClick={() => setUseClamp((prev) => !prev)}
+              >
+                {useClamp ? '더보기' : '접기'}
+              </button>
+            )}
+          </div>
+          <div className="mt-4 flex w-full items-center justify-between gap-4">
+            <p className="line-clamp-1 text-12 font-500 leading-[160%] text-basic-grey-400">
+              {review.eventName}
+            </p>
+            <p className="text-12 font-500 leading-[160%] text-basic-grey-500">
+              {formatDateString(review.createdAt, 'date')}
+            </p>
+          </div>
+        </div>
+      </article>
+      {openImageUrl && (
+        <ImageModal
+          imageUrl={openImageUrl}
+          onClose={() => setOpenImageUrl(null)}
+        />
+      )}
+    </>
+  );
+};
+
+export default ReviewItem;
---
+++ src/app/reviews/components/ReviewList.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import ReviewItem from './ReviewItem';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import { useGetReviewsWithPagination } from '@/services/review.service';
+import FilterButton from './FilterButton';
+import { useMemo, useState } from 'react';
+import ReviewStatistics from './ReviewStatistics';
+import Image from 'next/image';
+import ReviewBanner from '../images/review-banner.png';
+
+export type ReviewSortType = 'DATE_DESC' | 'RATING_DESC' | 'RATING_ASC';
+
+const ReviewList = () => {
+  const [sort, setSort] = useState<ReviewSortType>('RATING_DESC');
+  const {
+    data: reviewPages,
+    fetchNextPage,
+    isFetching,
+    hasNextPage,
+  } = useGetReviewsWithPagination({
+    orderBy: sort === 'DATE_DESC' ? undefined : 'rating',
+    additionalOrderOptions:
+      sort === 'DATE_DESC'
+        ? undefined
+        : sort === 'RATING_DESC'
+          ? 'DESC'
+          : 'ASC',
+  });
+  const reviews = useMemo(() => {
+    return reviewPages?.pages.flatMap((page) => page.reviews) ?? [];
+  }, [reviewPages]);
+
+  const { InfiniteScrollTrigger } = useInfiniteScroll({
+    fetchNextPage,
+    isLoading: isFetching,
+    hasNextPage,
+  });
+
+  return (
+    <>
+      <Image src={ReviewBanner} alt="핸디버스 후기" />
+      <ReviewStatistics />
+      <div className="h-8 bg-basic-grey-50" />
+      <section className="mt-32 flex flex-col gap-16 px-16">
+        <FilterButton sort={sort} onSort={setSort} />
+        {reviews.map((review) => (
+          <ReviewItem key={review.reviewId} review={review} />
+        ))}
+        <InfiniteScrollTrigger />
+      </section>
+    </>
+  );
+};
+
+export default ReviewList;
