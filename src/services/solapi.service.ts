0348c5a6ce3ef02ae600915ba661d79826dddbb2
Geon <geonulim@gmail.com>
2025-11-05 14:09:08 +0900

    remove: 미사용 솔라피 api 삭제

--- src/services/solapi.service.ts
+++ src/services/solapi.service.ts
@@ -1,32 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { authInstance } from './config';
 
-export const sendDemandedShuttleRouteNotCreated = async (eventId: string) => {
-  await authInstance.post(
-    `/v1/shuttle-operation/admin/events/${eventId}/send-demanded-shuttle-route-not-created`,
-  );
-};
-
-export const useSendDemandedShuttleRouteNotCreated = () => {
-  return useMutation({
-    mutationFn: ({ eventId }: { eventId: string }) =>
-      sendDemandedShuttleRouteNotCreated(eventId),
-  });
-};
-
-export const sendDemandedShuttleRouteDoneCreating = async (eventId: string) => {
-  await authInstance.post(
-    `/v1/shuttle-operation/admin/events/${eventId}/send-demanded-shuttle-route-done-creating`,
-  );
-};
-
-export const useSendDemandedShuttleRouteDoneCreating = () => {
-  return useMutation({
-    mutationFn: ({ eventId }: { eventId: string }) =>
-      sendDemandedShuttleRouteDoneCreating(eventId),
-  });
-};
-
 export const sendShuttleBusCancelled = async ({
   reservationIds,
   shuttleRouteId,
