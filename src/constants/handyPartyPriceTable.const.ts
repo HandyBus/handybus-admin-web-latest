438daa7889d5663fb2dea4e3dbd2a82dc5523672
jujeon <jwjsjwnsstjd@gmail.com>
2026-01-23 17:15:09 +0900

    fix: GOYANG_STADIUM_PRICE_TABLE 업데이트

--- src/constants/handyPartyPriceTable.const.ts
+++ src/constants/handyPartyPriceTable.const.ts
@@ -6,92 +6,92 @@ export type HandyPartyPriceTable = {
   earlybirdPrice: number;
 }[];
 
-// 고양종합운동장 25.06
+// 고양종합운동장 26.01.23 수정
 export const GOYANG_STADIUM_PRICE_TABLE: HandyPartyPriceTable = [
   {
     area: '동북권',
-    regularPrice: 34300,
-    earlybirdPrice: 30000,
+    regularPrice: 28800,
+    earlybirdPrice: 26000,
   },
   {
     area: '서북권',
-    regularPrice: 25700,
-    earlybirdPrice: 22500,
-  },
-  {
-    area: '중심권',
-    regularPrice: 28600,
-    earlybirdPrice: 25000,
-  },
-  {
-    area: '성동.광진권',
-    regularPrice: 34300,
-    earlybirdPrice: 30000,
-  },
-  {
-    area: '강서권',
-    regularPrice: 20000,
-    earlybirdPrice: 17500,
-  },
-  {
-    area: '서남권',
-    regularPrice: 28600,
-    earlybirdPrice: 25000,
-  },
-  {
-    area: '동남권',
-    regularPrice: 34300,
-    earlybirdPrice: 30000,
-  },
-  {
-    area: '수원',
-    regularPrice: 37100,
-    earlybirdPrice: 32500,
-  },
-  {
-    area: '성남',
-    regularPrice: 37100,
-    earlybirdPrice: 32500,
-  },
-  {
-    area: '고양',
-    regularPrice: 20000,
-    earlybirdPrice: 17500,
-  },
-  {
-    area: '부평',
-    regularPrice: 21400,
-    earlybirdPrice: 18750,
-  },
-  {
-    area: '광명',
-    regularPrice: 22800,
+    regularPrice: 21600,
     earlybirdPrice: 20000,
   },
   {
+    area: '중심권',
+    regularPrice: 26400,
+    earlybirdPrice: 24000,
+  },
+  {
+    area: '성동.광진권',
+    regularPrice: 28800,
+    earlybirdPrice: 26000,
+  },
+  {
+    area: '강서권',
+    regularPrice: 24000,
+    earlybirdPrice: 22000,
+  },
+  {
+    area: '서남권',
+    regularPrice: 26400,
+    earlybirdPrice: 24000,
+  },
+  {
+    area: '동남권',
+    regularPrice: 31200,
+    earlybirdPrice: 29000,
+  },
+  {
+    area: '수원',
+    regularPrice: 36000,
+    earlybirdPrice: 33000,
+  },
+  {
+    area: '성남',
+    regularPrice: 33600,
+    earlybirdPrice: 31000,
+  },
+  {
+    area: '고양',
+    regularPrice: 16800,
+    earlybirdPrice: 15400,
+  },
+  {
+    area: '부평',
+    regularPrice: 22800,
+    earlybirdPrice: 21000,
+  },
+  {
+    area: '광명',
+    regularPrice: 20000,
+    earlybirdPrice: 18000,
+  },
+  {
     area: '동탄',
-    regularPrice: 37100,
-    earlybirdPrice: 0, // 얼리버드 없음
+    regularPrice: 36300,
+    earlybirdPrice: 33000,
   },
   {
     area: '안양',
-    regularPrice: 25700,
-    earlybirdPrice: 0, // 얼리버드 없음
+    regularPrice: 30000,
+    earlybirdPrice: 28000,
   },
   {
     area: '안산',
-    regularPrice: 37100,
-    earlybirdPrice: 32500,
+    regularPrice: 32500,
+    earlybirdPrice: 30000,
   },
   {
     area: '하남',
-    regularPrice: 40000,
-    earlybirdPrice: 35000,
+    regularPrice: 33600,
+    earlybirdPrice: 31000,
   },
   {
     area: '남양주',
-    regularPrice: 37100,
-    earlybirdPrice: 32500,
+    regularPrice: 32500,
+    earlybirdPrice: 30000,
   },
   {
     area: '용인',
